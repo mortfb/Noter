e46ccea32ec99e23c7135001ab4e76da14ecd19f
Morten <mofb@itu.dk>
2026-01-21 14:10:24 +0100

    feat: add menu

--- MyNotePad/MyNotePad/MyForm.cs
+++ MyNotePad/MyNotePad/MyForm.cs
@@ -2,8 +2,40 @@ namespace MyNotePad;
 
 public partial class MyForm : Form
 {
+    private RichTextBox _editor;
+    private string _currentFile = "";
     public MyForm()
     {
-        InitializeComponent();
+        Text = "My Notepad";
+        //These can be altered
+        Width = 1000;
+        Height = 800;
+
+        _editor = new RichTextBox
+        {
+            Dock = DockStyle.Fill, Font = new Font("Times New Roman", 11), WordWrap = true
+        };
+
+        var menu = new MenuStrip();
+        var menuFiles = new ToolStripMenuItem("File");
+        var menuNewFile = new ToolStripMenuItem("New File", null, NewFile);
+        var menuOpenFile = new ToolStripMenuItem("Open", null, OpenFile);
+        var menuSaveFile = new ToolStripMenuItem("Save", null, SaveFile);
+        var menuSaveAsItem = new ToolStripMenuItem("Save As", null, SaveAsFile);
+        var menuExit = new ToolStripMenuItem("Exit", null, (_, _) => Close());
+        
+        //Add the options to the file tab
+        menuFiles.DropDownItems.Add(menuNewFile);
+        menuFiles.DropDownItems.Add(menuOpenFile);
+        menuFiles.DropDownItems.Add(menuSaveFile);
+        menuFiles.DropDownItems.Add(menuSaveAsItem);
+        menuFiles.DropDownItems.Add(menuExit);
+        
+        //add the filetab to the menu
+        menu.Items.Add(menuFiles);
+        
+        Controls.Add(_editor);
+        Controls.Add(menu);
+        //InitializeComponent();
     }
 }
